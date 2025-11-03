4adb030990cc93e9c92b71153c36127188102cc9
Diego <dpadula@epe.santafe.gov.ar>
2025-11-03 12:24:53 -0300

    esto no anda

---
+++ app/index.tsx
@@ -0,0 +1,43 @@
+import { StyleSheet, View } from 'react-native';
+
+import { MapView } from '@maplibre/maplibre-react-native';
+
+export default function Index() {
+  const MAPTILER_API_KEY = 'cVxKrO7kxSIAfQTy4QJP';
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.mapcontainer}>
+        <MapView
+          style={styles.map}
+          mapStyle={`https://api.maptiler.com/maps/streets-v2/style.json?key=${MAPTILER_API_KEY}`}
+          logoEnabled={false}
+          attributionPosition={{ bottom: 8, right: 8 }}
+        >
+          {/* <Camera
+            centerCoordinate={[2, 41.5]}
+            zoomLevel={8}
+            animationDuration={2000}
+            // animationMode='easeTo'
+          /> */}
+        </MapView>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mapcontainer: {
+    width: '100%',
+    height: '100%',
+  },
+  map: {
+    flex: 1,
+  },
+});
