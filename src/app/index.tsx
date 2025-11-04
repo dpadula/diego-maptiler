5fcbae46859ff49ce7c2c5970685635f3b1af0f0
Diego <dpadula@epe.santafe.gov.ar>
2025-11-04 09:28:47 -0300

    Alguna componentizacion util

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,14 +2,24 @@ import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 import { Camera, MapView } from '@maplibre/maplibre-react-native';
 import { useState } from 'react';
+import FloatingButtons from '../components/FloatingButtons';
 
 export default function Index() {
   const MAPTILER_API_KEY = 'cVxKrO7kxSIAfQTy4QJP';
-  const [layer, setLayer] = useState<string>(
-    'https://api.maptiler.com/maps/satellite/'
+  const [mapStyle, setMapStyle] = useState<string>(
+    'https://api.maptiler.com/maps/streets-v2/style.json?key=' +
+      MAPTILER_API_KEY
   );
   const [zoom, setZoom] = useState(16);
-
+  // mapStyle={`https://api.maptiler.com/maps/streets-v4/style.json?key=${MAPTILER_API_KEY}`}
+  // mapStyle={`https://api.maptiler.com/maps/satellite/style.json?key=${MAPTILER_API_KEY}`}
+  const toggleMapStyle = () => {
+    setMapStyle((prev) =>
+      prev.includes('streets')
+        ? `https://api.maptiler.com/maps/satellite/style.json?key=${MAPTILER_API_KEY}`
+        : `https://api.maptiler.com/maps/streets-v2/style.json?key=${MAPTILER_API_KEY}`
+    );
+  };
   return (
     <View style={styles.container}>
       <View style={styles.mapcontainer}>
@@ -17,9 +27,7 @@ export default function Index() {
           // onPress={(feature) => console.log('pressed', feature)}
           onLongPress={(feature) => console.log('pressed', feature)}
           style={styles.map}
-          mapStyle={`${layer}style.json?key=${MAPTILER_API_KEY}`}
-          // mapStyle={`https://api.maptiler.com/maps/streets-v4/style.json?key=${MAPTILER_API_KEY}`}
-          // mapStyle={`https://api.maptiler.com/maps/satellite/style.json?key=${MAPTILER_API_KEY}`}
+          mapStyle={mapStyle}
           logoEnabled={false}
           attributionPosition={{ bottom: 8, right: 8 }}
         >
@@ -27,30 +35,10 @@ export default function Index() {
             centerCoordinate={[-60.688798780339226, -31.635692179155193]}
             zoomLevel={zoom}
             animationDuration={1500}
-            animationMode='easeTo'
+            animationMode='flyTo'
           />
         </MapView>
-        <Pressable
-          style={{
-            position: 'absolute',
-            top: 50,
-            zIndex: 1000,
-            backgroundColor: 'white',
-            padding: 20,
-            marginHorizontal: 10,
-            borderRadius: 5,
-          }}
-          onPress={() => {
-            if (layer.includes('satellite')) {
-              setLayer('https://api.maptiler.com/maps/openstreetmap/');
-              // setLayer('https://api.maptiler.com/maps/streets-v4/');
-            } else {
-              setLayer('https://api.maptiler.com/maps/satellite/');
-            }
-          }}
-        >
-          <Text>Cambiar Satelite</Text>
-        </Pressable>
+
         <Pressable
           style={{
             position: 'absolute',
@@ -71,6 +59,12 @@ export default function Index() {
         >
           <Text>Cambiar Zoom</Text>
         </Pressable>
+
+        <FloatingButtons
+          onToggleStyle={toggleMapStyle}
+          onZoomIn={() => setZoom((z) => Math.min(z + 1, 20))}
+          onZoomOut={() => setZoom((z) => Math.max(z - 1, 3))}
+        />
       </View>
     </View>
   );
---
+++ src/components/FloatingButtons.tsx
@@ -0,0 +1,58 @@
+import { FontAwesome, FontAwesome6 } from '@expo/vector-icons';
+import React from 'react';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+
+type FloatingButtonProps = {
+  onToggleStyle: () => void;
+  onZoomIn: () => void;
+  onZoomOut: () => void;
+};
+
+const FloatingButtons = ({
+  onToggleStyle,
+  onZoomIn,
+  onZoomOut,
+}: FloatingButtonProps) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button} onPress={onToggleStyle}>
+        <FontAwesome name='map' size={22} color='black' />
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button} onPress={onZoomIn}>
+        <FontAwesome6 name='plus' size={22} color='black' />
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button} onPress={onZoomOut}>
+        {/* <Ionicons name='remove' size={22} color='black' style={styles.text} /> */}
+        <FontAwesome6 name='minus' size={22} color='black' />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default FloatingButtons;
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    right: 20,
+    bottom: 100,
+    gap: 10,
+  },
+  button: {
+    backgroundColor: '#fff',
+    padding: 15,
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 4,
+  },
+  text: {
+    fontSize: 20,
+    color: '#000',
+    fontWeight: 700,
+  },
+});
